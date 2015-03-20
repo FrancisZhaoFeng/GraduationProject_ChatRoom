a2a810ccc4ebfea18d2428b00bc3cefa01aa3d25
FrancisZhaoFeng <zgfailmr@163.com>
2015-03-20 22:07:09 +0800

    随便修改一个文件查看github效果
    test

--- chatRoom-master/github-code/Client/src/org/yuner/www/myNetwork/NetConnect.java
+++ chatRoom-master/github-code/Client/src/org/yuner/www/myNetwork/NetConnect.java
@@ -26,100 +26,78 @@ import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class NetConnect extends Thread{
-	
-	/*  information about Server, ip address and portal number  */
-//	private String mHostIp="54.251.178.242";
-//	private String mHostIp="192.168.208.128";
-	private String mHostIp="172.16.15.101";
+public class NetConnect extends Thread {
 
-	private int mHostPort=8888;
-	
-	/*  socket  */
-	private Socket mSocket0=null;
-	
-	private boolean connectedAlready=false;
-	
-	/*  singleton retrieval  
-	public static NetConnect getnetConnect() {
-		if(netConnect == null) {
-			netConnect = new NetConnect();
-		}
-		return netConnect;
+	/* information about Server, ip address and portal number */
+	// private String mHostIp="54.251.178.242";
+	// private String mHostIp="192.168.208.128";
+	private String mHostIp = "172.16.15.101";
+
+	private int mHostPort = 8888;
+
+	/* socket */
+	private Socket mSocket0 = null;
+
+	private boolean connectedAlready = false;
+
+	/*
+	 * singleton retrieval public static NetConnect getnetConnect() {
+	 * if(netConnect == null) { netConnect = new NetConnect(); } return
+	 * netConnect; }
+	 */
+
+	public NetConnect() {
+
 	}
-	*/
-	
-	public NetConnect()
-	{
-		
-	}
-	
-	public void run()
-	{
+
+	public void run() {
 		try {
 			sleep(100); // to wait for the connection to be stable
-		} catch(Exception e) {}
-		try{		
+		} catch (Exception e) {
+		}
+		try {
 			// initialization and establish connection
-			InetAddress ia0=InetAddress.getByName(mHostIp);
-			InetSocketAddress ist0=new InetSocketAddress(ia0,mHostPort);
-			mSocket0=new Socket();
-			mSocket0.connect(ist0,2000);
-			
-			if(mSocket0.isConnected()) {
+			InetAddress ia0 = InetAddress.getByName(mHostIp);
+			InetSocketAddress ist0 = new InetSocketAddress(ia0, mHostPort);
+			mSocket0 = new Socket();
+			mSocket0.connect(ist0, 2000);
+
+			if (mSocket0.isConnected()) {
 				connectedAlready = true;
 			} else {
 				connectedAlready = false;
 			}
-		}catch(IOException e){
+		} catch (IOException e) {
 			System.out.println("error occured");
 		}
 	}
-	
+
 	public Socket getSocket() {
 		return mSocket0;
 	}
-	
-	public boolean connectedOrNot()
-	{
+
+	public boolean connectedOrNot() {
 		return connectedAlready;
 	}
-	
+
 	/*
-	public void startListen(Context context0) {
-		mClientListen0 = new ClientListenThread(context0,mSocket0);
-		mClientListen0.start();
-		
-		mClientSend0 = new ClientSendThread();
-	}
-	
-	public void sendUpload(int type, String sentence)
-	{
-		sendUpload(type + "");
-		sendUpload(sentence);
-	}
-	
-	private synchronized void sendUpload(String buff)
-	{
-		buff = buff.replace("\n", GlobalStrings.replaceOfReturn);
-		mClientSend0.start(mSocket0,buff);
-	}
-	
-	
-	
-	public void closeNetConnect()
-	{
-		try{
-			if(mClientListen0 != null) {
-				mClientListen0.closeBufferedReader();
-			}
-		} catch (Exception e) {}
-		try{
-			if(mSocket0 != null) {
-				mSocket0.close();
-			}
-		} catch (Exception e) {}
-		netConnect=null;
-	}
-	*/
+	 * public void startListen(Context context0) { mClientListen0 = new
+	 * ClientListenThread(context0,mSocket0); mClientListen0.start();
+	 * 
+	 * mClientSend0 = new ClientSendThread(); }
+	 * 
+	 * public void sendUpload(int type, String sentence) { sendUpload(type +
+	 * ""); sendUpload(sentence); }
+	 * 
+	 * private synchronized void sendUpload(String buff) { buff =
+	 * buff.replace("\n", GlobalStrings.replaceOfReturn);
+	 * mClientSend0.start(mSocket0,buff); }
+	 * 
+	 * 
+	 * 
+	 * public void closeNetConnect() { try{ if(mClientListen0 != null) {
+	 * mClientListen0.closeBufferedReader(); } } catch (Exception e) {} try{
+	 * if(mSocket0 != null) { mSocket0.close(); } } catch (Exception e) {}
+	 * netConnect=null; }
+	 */
 }
